fc03f53da417ec4e50dfdb243a03fbc83f25eeb6
Vincenzo-Cuccaro <enzocuccaro03@gmail.com>
2026-09-10 14:39:25 +0200

    Refactor plotConvergence function for clarity

--- plotConvergence.m
+++ plotConvergence.m
@@ -1,12 +1,4 @@
 function [A] = plotConvergence(seed, iter, ifs, plotShape)
-% seed: insieme poligonale definito mediante polyshape. Costituisce
-% l'insieme su cui operare l'algoritmo descritto nel teorema delle
-% contrazioni
-% iter: insieme di indici naturali (compreso lo 0) in ordine crescente.
-% Descrive le iterate dell'algoritmo che si vuole andare a graficare
-% ifs: vettore di mappe affini. Ciascuna mappa f_i è descritta da una
-% matrice ifs([1 2], [1 2], i) e un vettore di traslazione ifs(3, [1 2], i)
-% plotShape: un vettore bidimensionale che specifica i parametri di subplot
 
 figure();
 hold on;
@@ -21,7 +13,7 @@ if ismember(0, iter)
     axis equal;
     xlim([0, 1]);
     ylim([0, 1]);
-    title("Seme dell'algoritmo");
+    title("Algorithm seed");
     set(gca,'XTick',[], 'YTick', [])
 end
 
@@ -41,7 +33,7 @@ for i = 1 : n
         axis equal;
         xlim([0, 1]);
         ylim([0, 1]);
-        title(['Iterata ', num2str(i)]);
+        title(['Iteration ', num2str(i)]);
         set(gca,'XTick',[], 'YTick', [])
     end
 end
